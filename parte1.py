f7bb65e3bd99ba1652e50514f6a6fce8591be6a6
alonsonhernandez <alonso.nhernandez@alumnos.upm.es>
2025-01-25 20:03:48 +0100

    parte1.py

--- parte1.py
+++ parte1.py
@@ -11,26 +11,9 @@ os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
 variable = os.environ.get('GROUP_NUMBER')      
 print("Valor de GROUP_NUMBER:", variable)
 
-fin = open("/practica_creativa2/bookinfo/src/productpage/templates/index.html", 'r') 
+fin = open("/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/index.html", 'r') 
 
-fout = open("/practica_creativa2/bookinfo/src/productpage/templates/index2.html", 'w')
-
-for line in fin:
-  if "{% block title %}Simple Bookstore App{% endblock %}" in line :
-   fout.write("{% block title %}"+"Simple Bookstore App {}".format(variable) + "{% endblock %}")
-  else:
-   fout.write(line)
-fin.close()
-fout.close()
-
-
-call(["cp","/practica_creativa2/bookinfo/src/productpage/templates/index2.html","/home/practica_creativa2/bookinfo/src/productpage/templates/index.html"])
-call(["rm","/practica_creativa2/bookinfo/src/productpage/templates/index2.html"])
-
-
-fin = open("/practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'r') 
-
-fout = open("/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html", 'w') 
+fout = open("/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/index2.html", 'w')
 
 for line in fin:
   if "{% block title %}Simple Bookstore App{% endblock %}" in line :
@@ -41,14 +24,31 @@ fin.close()
 fout.close()
 
 
-call(["cp","/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html","/practica_creativa2/bookinfo/src/productpage/templates/productpage.html"])
-call(["rm","/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html"])
+call(["cp","/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/index2.html","/home/practica_creativa2/bookinfo/src/productpage/templates/index.html"])
+call(["rm","/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/index2.html"])
+
+
+fin = open("/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'r') 
+
+fout = open("/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html", 'w') 
+
+for line in fin:
+  if "{% block title %}Simple Bookstore App{% endblock %}" in line :
+   fout.write("{% block title %}"+"Simple Bookstore App {}".format(variable) + "{% endblock %}")
+  else:
+   fout.write(line)
+fin.close()
+fout.close()
+
+
+call(["cp","/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html","/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage.html"])
+call(["rm","/home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html"])
 
 
 os.system('sudo apt-get update')
 os.system('sudo apt-get install  python3-pip')
-os.system('pip3 install -r /practica_creativa2/bookinfo/src/productpage/requirements.txt')
-os.system(' python3 /practica_creativa2/bookinfo/src/productpage/productpage_monolith.py 8080')
+os.system('pip3 install -r /home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/requirements.txt')
+os.system(' python3 /home/cdps/parte1/practica_creativa2/bookinfo/src/productpage/productpage_monolith.py 8080')
 
 
 
