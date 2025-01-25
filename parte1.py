714ea6f074ef29893af0aa3a6edb59629aee359d
alonsonhernandez <alonso.nhernandez@alumnos.upm.es>
2025-01-25 19:46:17 +0100

    parte1.py

--- parte1.py
+++ parte1.py
@@ -5,14 +5,15 @@ import sys
 import json
 import time
 
+
 from subprocess import call
 os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
 variable = os.environ.get('GROUP_NUMBER')      
 print("Valor de GROUP_NUMBER:", variable)
 
-fin = open("/home/practica_creativa2/bookinfo/src/productpage/templates/index.html", 'r') 
+fin = open("/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/index.html", 'r') 
 
-fout = open("/home/practica_creativa2/bookinfo/src/productpage/templates/index2.html", 'w')
+fout = open("/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/index2.html", 'w')
 
 for line in fin:
   if "{% block title %}Simple Bookstore App{% endblock %}" in line :
@@ -23,12 +24,14 @@ fin.close()
 fout.close()
 
 
-call(["cp","/home/practica_creativa2/bookinfo/src/productpage/templates/index2.html","/home/practica_creativa2/bookinfo/src/productpage/templates/index.html"])
-call(["rm","/home/practica_creativa2/bookinfo/src/productpage/templates/index2.html"])
+call(["cp","/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/index2.html","/home/practica_creativa2/bookinfo/src/productpage/templates/index.html"])
+call(["rm","/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/index2.html"])
 
 
-fin = open("/home/practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'r') 
-fout = open("/home/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html", 'w') 
+fin = open("/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'r') 
+
+fout = open("/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html", 'w') 
+
 for line in fin:
   if "{% block title %}Simple Bookstore App{% endblock %}" in line :
    fout.write("{% block title %}"+"Simple Bookstore App {}".format(variable) + "{% endblock %}")
@@ -38,14 +41,20 @@ fin.close()
 fout.close()
 
 
-call(["cp","/home/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html","/home/cdps/Parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage.html"])
-call(["rm","/home/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html"])
+call(["cp","/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html","/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage.html"])
+call(["rm","/home/parte1/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html"])
 
 
 os.system('sudo apt-get update')
 os.system('sudo apt-get install  python3-pip')
-os.system('pip3 install -r /home/practica_creativa2/bookinfo/src/productpage/requirements.txt')
-os.system(' python3 /home/practica_creativa2/bookinfo/src/productpage/productpage_monolith.py 8080')
+os.system('pip3 install -r /home/parte1/practica_creativa2/bookinfo/src/productpage/requirements.txt')
+os.system(' python3 /home/parte1/practica_creativa2/bookinfo/src/productpage/productpage_monolith.py 8080')
+
+
+
+
+
+
 
 
 
